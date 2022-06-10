e7c7af679734111b9d81dcf41958620c851adf6c
Jaeheon Shim <git@jaeheonshim.com>
2022-06-10 17:10:23 -0400

    Add Google OAuth login

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cron = require("node-cron")
 
+// MIDDLEWARES
+const session = require("express-session");
+
+// SERVICES
 const dbservice = require("./services/dbservice");
 
 // ROUTES
@@ -12,6 +16,12 @@ const weather = require("./routes/weather");
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "SECRET"
+}));
 
 async function main() {
     await mongoose.connect(process.env.MONGODB_ATLAS);
@@ -36,3 +46,49 @@ app.post("/find/name/:query", beaches.nameSearch);
 app.post("/info/:id", beaches.getInfo);
 
 app.post("/weather/:id", weather.getWeather);
+
+app.get("/session", function(req, res) {
+    res.send(req.session);
+});
+
+/* PASSPORT OAUTH */
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_OAUTH_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_OAUTH_SECRET;
+
+const passport = require("passport");
+let userProfile;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/error", (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:8080/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        userProfile = profile;
+        return done(null, userProfile);
+    }
+));
+
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile"]}));
+app.get("/auth/google/callback", 
+    passport.authenticate("google", 
+    { failureRedirect: "/error"}),
+    function(req, res) {
+        res.redirect("/session");
+    });
